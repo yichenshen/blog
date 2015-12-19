fee77c096d77c8856891fe040faa47605cbebf97
Shen Yichen <2007.yichen@gmail.com>
2015-12-19 18:05:18 +0800

    Add method for post model
    Added method to list, show, create, and edit blog posts in database to the post model.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/application/model/post.php
+++ src/application/model/post.php
@@ -8,11 +8,71 @@
 
 class Post extends Model{
 	
+	/**
+	 * Returns all the posts objects
+	 */
 	function all() {
-        $sql = "SELECT title, content, user_username as 'author' FROM post";
+        $sql = "SELECT * FROM post";
         $query = $this->db->prepare($sql);
         $query->execute();
 
         return $query->fetchAll();
+    }
+
+    /**
+     * Returns a single blog post object, by ID.
+     *
+     * @param	int		$id			The ID of the blog post to return.
+     */
+    function get($id){
+    	$sql  = "SELECT * FROM post WHERE post_id = :id";
+		$query = $this->db->prepare($sql);
+    	$parameters = array(':id' => $id);
+
+    	$query->execute($parameters);
+
+    	return $query->fetch();
+    }
+
+    /**
+     * Creates a new blog post in the database, together with content.
+     * 
+     * The author of the post is assigned based on login.
+     *
+     * @param	string	$title		The title of the blog post
+     * @param	string	$content	The content of the blog post 
+     */
+    function create($title, $content){
+		$sql = "INSERT INTO post (title, content, user_username) VALUES (:title, :content, :author)";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':title' => $title, ':content' => $content, ':author' => "admin"); //TODO: replace with loggedInUser
+    
+    	$query->execute($parameters);
+    }
+
+    /**
+     * Edit the title and contents of a blog post.
+	 *
+     * @param	int		$id			The ID of the blog post to edit
+     * @param	string	$title		The title of the blog post
+     * @param	string	$content	The content of the blog post 
+     */
+    function edit($id, $title, $content){
+		$sql = "UPDATE post SET title = :title, content = :content, update_time = now() WHERE post_id = :id";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':id' => $id, ':title' => $title, ':content' => $content);
+
+        $query->execute($parameters);
+    }
+
+    /**
+     * Deletes the blog post.
+     * 
+     * @param	int		$id			The ID of the blog post to delete.
+     */
+    function delete($id){
+    	$sql = "DELETE FROM post WHERE post_id = :id";
+    	$query = $this->db->prepare($sql);
+    	$parameters = array(':id' =>)
     }
 }
